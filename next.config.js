89bc3e1235978be9baf988fb051fb120215331aa
Alan Abilio <alandocarmoabilio@gmail.com>
2023-11-18 15:17:06 -0300

    build: set apiKey

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
+const dotenv = require("dotenv");
+dotenv.config();
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = {
+  env: {
+    TMDB_API_KEY: process.env.TMDB_API_KEY,
+  },
+};
