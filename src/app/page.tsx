21210bfed1d847b1cc96d26cc5808bcb71ac2d3c
Alan Abilio <aladocarmoabilio@gmail.com>
2024-01-30 17:52:34 -0300

    fix: remove redundancy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import PopularMovies from "@/app/Popular/page";
-import IntallPwaComponent from "@/components/InstallPWA";
 
 export default async function Home() {
   return (
     <>
       <PopularMovies />
-      <IntallPwaComponent />
     </>
   );
 }
